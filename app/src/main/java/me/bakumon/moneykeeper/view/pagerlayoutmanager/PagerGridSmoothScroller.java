4c456bb102c53e96c86a51b1bbf5e0a8e469cf67
Bakumon <bakumon.me@gmail.com>
2018-06-11 14:34:19 +0800

    💰 license

--- app/src/main/java/me/bakumon/moneykeeper/view/pagerlayoutmanager/PagerGridSmoothScroller.java
+++ app/src/main/java/me/bakumon/moneykeeper/view/pagerlayoutmanager/PagerGridSmoothScroller.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 GcsSloop
+ * Copyright 2018 Bakumon. https://github.com/Bakumon
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -11,13 +11,7 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * Last modified 2018-04-09 23:56:59
- *
- * GitHub: https://github.com/GcsSloop
- * WeiBo: http://weibo.com/GcsSloop
- * WebSite: http://www.gcssloop.com
+ *  limitations under the License.
  */
 
 package me.bakumon.moneykeeper.view.pagerlayoutmanager;
